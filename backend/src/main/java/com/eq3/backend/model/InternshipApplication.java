6c5cf3de8550c01e2d0b763a15df62c684a6b6e8
Renaud Vincent <v.pisano35@gmail.com>
2021-10-26 14:19:52 -0400

    Vue - ajout backend

---
+++ backend/src/main/java/com/eq3/backend/model/InternshipApplication.java
@@ -0,0 +1,37 @@
+package com.eq3.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+
+@Data
+@SuperBuilder(toBuilder = true)
+@AllArgsConstructor
+public class InternshipApplication extends Entity{
+
+    public enum ApplicationStatus {
+        NOT_ACCEPTED,
+        ACCEPTED,
+        WAITING,
+        VALIDATED,
+        COMPLETED
+    }
+
+    @Builder.Default
+    private ApplicationStatus status = ApplicationStatus.WAITING;
+
+    @DBRef
+    private InternshipOffer internshipOffer;
+
+    @DBRef
+    private Student student;
+
+    public InternshipApplication() {
+        super();
+        this.status = ApplicationStatus.WAITING;
+    }
+}
---
+++ backend/src/main/java/com/eq3/backend/repository/InternshipRepository.java
@@ -0,0 +1,13 @@
+package com.eq3.backend.repository;
+
+import com.eq3.backend.model.Internship;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface InternshipRepository extends MongoRepository<Internship, String> {
+
+    Optional<Internship> findByInternshipApplication_Id(String id);
+
+}
