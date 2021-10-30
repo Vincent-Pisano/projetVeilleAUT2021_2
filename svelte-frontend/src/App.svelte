7bb0052d0b0e05d7db1095bc7bbfa238f3b2ceeb
Renaud Vincent <v.pisano35@gmail.com>
2021-10-30 12:23:52 -0400

    Ajout gestion router part 1

--- svelte-frontend/src/App.svelte
+++ svelte-frontend/src/App.svelte
@@ -1,30 +1,23 @@
+
+
 <script>
-	export let name;
-</script>
+	import { Router, Route } from "svelte-routing";
+	import Home from "./components/Home.svelte";
+	import Login from "./components/Auth/Login/Login.svelte";
+	import Subscribe from "./components/Auth/Subscribe/Subscribe.svelte";
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+	export let url = "";
+  </script>
+  
+  <Router {url}>
+	<div>
+	  <Route path="/" component={Login} />
+	  <Route path="/subscribe" component={Subscribe} />
+	  <Route path="/home" component={Home} />
+	  <Route component={Login} />
+	</div>
+  </Router>
+  
+  <style>
+  </style>
+  
