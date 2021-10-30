96470f7845363d691c1c140cd79660e6cc035cac
Renaud Vincent <v.pisano35@gmail.com>
2021-10-30 14:03:07 -0400

    Utilisation Bootstrap (tentative version 2)

--- svelte-frontend/src/components/Auth/Login/Login.svelte
+++ svelte-frontend/src/components/Auth/Login/Login.svelte
@@ -1,6 +1,6 @@
-<div class="container mt-3">
-    <h1 class="">Login</h1>
+<div class="container mt-3 bg-grey">
+    <h1>Login</h1>
 
-    <button class="btn btn-lg btn-sucess">Oui</button>
+    <button class="btn btn-lg btn-warning">Oui</button>
 </div>
 
