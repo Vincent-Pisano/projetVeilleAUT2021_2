500e0bc696c22878950db10b659b5f5f00659967
Renaud Vincent <v.pisano35@gmail.com>
2021-10-31 16:28:49 -0400

    Gestion Bootstrap partie 1

--- vue-frontend/src/components/Navbar/Navbar.vue
+++ vue-frontend/src/components/Navbar/Navbar.vue
@@ -1,3 +1,38 @@
 <template>
-    <h1>Navbar</h1>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light px-3 m-0">
+    <span>Gestionnaire de stage</span>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarcontent"
+      aria-controls="navbarcontent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon" />
+    </button>
+
+    <div class="collapse navbar-collapse ml" id="navbarcontent">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link class="nav-link" to={{url.LOGIN}}>Connexion</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="{url.SUBSCRIBE}"
+            >Inscription</router-link
+          >
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
+
+<script>
+// eslint-disable-next-line no-unused-vars
+import URL from "../../utils/URL";
+
+export default {
+    name:'Navbar'
+};
+</script>
--- vue-frontend/src/main.js
+++ vue-frontend/src/main.js
@@ -1,7 +1,18 @@
+/* eslint-disable no-unused-vars */
 import { createApp } from 'vue'
 import App from './App.vue'
+import Vue from 'vue'
 import router from './router'
+import { BootstrapVue } from 'bootstrap-vue'
+
+// Import Bootstrap and Bootstrap-Vue css
+//import 'bootstrap/dist/css/bootstrap.css'
+//import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Install BootstrapVue
+//Vue.use(BootstrapVue)
 
 createApp(App)
+    //.use(BootstrapVue)
     .use(router)
     .mount('#app')
