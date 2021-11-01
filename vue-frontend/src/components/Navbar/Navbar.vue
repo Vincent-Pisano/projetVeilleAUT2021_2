acb7893f819839895f05983c5dc74594a2da9608
Renaud Vincent <v.pisano35@gmail.com>
2021-11-01 15:18:38 -0400

    Gestion de la navbar et clean up du code

--- vue-frontend/src/components/Navbar/Navbar.vue
+++ vue-frontend/src/components/Navbar/Navbar.vue
@@ -1,3 +1,39 @@
 <template>
-    <h1>Navbar</h1>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light px-3 m-0">
+    <span>Gestionnaire de stage</span>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarcontent"
+      aria-controls="navbarcontent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon" />
+    </button>
+
+    <div class="collapse navbar-collapse ml" id="navbarcontent">
+      <ul class="navbar-nav" :key="url.key" v-for="url in urls">
+        <li class="nav-item">
+          <router-link class="nav-link" :to="{ path: url.link }">
+            {{ url.name }}
+          </router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
+
+<script>
+import { HOME_PAGE_URL } from "../../utils/URL";
+
+export default {
+  name: "Navbar",
+  computed: {
+    urls() {
+      return HOME_PAGE_URL();
+    },
+  },
+};
+</script>
---
+++ vue-frontend/src/utils/URL.js
@@ -0,0 +1,44 @@
+function HOME_PAGE_URL() {
+    return [
+        {
+            key:"LOGIN",
+            link :"/", 
+            name:"Connexion"
+        },
+        {
+            key: "SUBSCRIBE",
+            link :"/subscribe", 
+            name:"Inscription"
+        }
+    ]
+}
+
+function IM_URL() {
+    return {
+        
+    }
+}
+
+
+function SUPERVISOR_URL() {
+    return {
+        
+    }
+}
+
+
+function STUDENT_URL() {
+    return {
+        
+    }
+}
+
+
+function MONITOR_URL() {
+    return {
+        
+    }
+}
+
+
+export { HOME_PAGE_URL, IM_URL, SUPERVISOR_URL, STUDENT_URL, MONITOR_URL }
