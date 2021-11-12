8bc4f59d1fb133ea03a26d9456aa469e73917f28
Renaud Vincent <v.pisano35@gmail.com>
2021-11-12 18:24:37 -0500

    Gestion des icons du status du CV

--- svelte-frontend/src/components/CVList/CVItem.svelte
+++ svelte-frontend/src/components/CVList/CVItem.svelte
@@ -1,26 +1,47 @@
 <script>
-    import CVButtonDownload from "./Button/CVButtonDownload.svelte";
-    import CVButtonDelete from "./Button/CVButtonDelete.svelte";
-    import CVButtonActive from "./Button/CVButtonActive.svelte";
-    export let cv;
+  import CVButtonDownload from "./Button/CVButtonDownload.svelte";
+  import CVButtonDelete from "./Button/CVButtonDelete.svelte";
+  import CVButtonActive from "./Button/CVButtonActive.svelte";
+  import Icon from "svelte-awesome";
+  import {
+    faCheck,
+    faTimes,
+    faSyncAlt,
+  } from "@fortawesome/free-solid-svg-icons";
+  export let cv;
 
-    let document = cv.pdfdocument
+  let document = cv.pdfdocument;
 </script>
 
 <tr>
-    <td>{document.name}</td>
-    <td>
-        <CVButtonDownload {cv}/>
-    </td>
-    <td>
-        <CVButtonDelete {cv}/>
-    </td>
-    <td>
-        <CVButtonActive {cv}/>
-    </td>
-    <td>ICON</td>
+  <td>{document.name}</td>
+  <td>
+    <CVButtonDownload {cv} />
+  </td>
+  <td>
+    <CVButtonDelete {cv} />
+  </td>
+  <td>
+    <CVButtonActive {cv} />
+  </td>
+  <td>
+    <Icon
+      data={cv.status === "VALID"
+        ? faCheck
+        : cv.status === "INVALID"
+        ? faTimes
+        : faSyncAlt}
+      style="{'color: ' + 
+        (cv.status == 'VALID'
+        ? 'green'
+        : cv.status === 'INVALID'
+        ? 'red'
+        : '#ffc107')};
+        width:50%;
+        height:50%"
+    />
+  </td>
 </tr>
 
 <style>
-    
 </style>
