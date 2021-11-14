ba2345564a057fb2037f88cbb1d964831c73f7d3
Renaud Vincent <v.pisano35@gmail.com>
2021-11-13 21:07:25 -0500

    Tentative modal

--- vue-frontend/src/components/CVList/Button/CVButtonDelete.vue
+++ vue-frontend/src/components/CVList/Button/CVButtonDelete.vue
@@ -1,29 +1,40 @@
 <template>
-  <button
-  class="btn btn-danger btn-sm"
-  type="button"
-  :click="() => (isOpen = !isOpen)"
->
-  Supprimer
+  <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Launch demo modal
 </button>
 
-<CVModalConfirmeDelete />
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
-import CVModalConfirmeDelete from "../Modal/CVModalConfirmeDelete.vue";
-
 export default {
   name: "CVButtonDownload",
   props: {
     cv: Object,
   },
-  components: {
-      CVModalConfirmeDelete
-  },
   data() {
     return {
-        isOpen: false
+        modalShow: false
     };
   },
 };
--- vue-frontend/src/components/CVList/Button/CVButtonDownload.vue
+++ vue-frontend/src/components/CVList/Button/CVButtonDownload.vue
@@ -1,17 +1,17 @@
 <template>
   <a
-      class="btn btn-success btn-sm"
-      :href="`${url}/${user.id}/${cv.id}`"
-      target="_blank"
-      rel="noreferrer"
-    >
-      Télécharger
-    </a>
+    class="btn btn-success btn-sm"
+    :href="`${url}/${user.id}/${cv.id}`"
+    target="_blank"
+    rel="noreferrer"
+  >
+    Télécharger
+  </a>
 </template>
 
 <script>
 import { URL_DOWNLOAD_CV } from "../../../utils/API";
-import auth from "../../../services/Auth"
+import auth from "../../../services/Auth";
 
 export default {
   name: "CVButtonDownload",
@@ -21,8 +21,8 @@ export default {
   data() {
     return {
       user: auth.user,
-      url: URL_DOWNLOAD_CV
+      url: URL_DOWNLOAD_CV,
     };
-  }
+  },
 };
 </script>
